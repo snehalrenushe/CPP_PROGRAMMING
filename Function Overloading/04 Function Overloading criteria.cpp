155f6da7c762ad46eb9541d91ae37ab1fde0b9a1
snehal renushe <snehalrenushe15@gmail.com>
2021-05-06 10:53:54 +0530

    Function Overloading

---
+++ Function Overloading/04 Function Overloading criteria.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+#include<iostream>
+using namespace std;
+
+class demo
+{
+    public:
+        void first(char cNo)
+        {
+            cout << "\n cNo as char is : " << cNo;
+        }
+
+        void first(int iNo)
+        {
+            cout << "\n iNo as integer is : " << iNo;
+        }
+
+        void first(double dNo)
+        {
+            cout << "\n dNo as double is : " << dNo;
+        }
+};
+
+int main()
+{
+    demo obj;
+
+    obj.first('S');
+    obj.first(15);
+    obj.first((int)92.67);
+
+    char ch = 'C';
+
+    obj.first(ch);
+
+    getch();
+    return 0;
+}
